0417f3dcf33524e3230459a9640fb6b6fbe3b85e
Mike Giovanetti <eth3real27@gmail.com>
2023-11-26 14:24:48 -0700

    dice roll

--- src/main.py
+++ src/main.py
@@ -1,26 +1,46 @@
 import os
+import random
 
 import discord
+from discord.ext import commands
 
 """
-Quick example of creating a basic bot
-From: Discord.py quickstart https://discordpy.readthedocs.io/en/stable/quickstart.html
+Quick example of creating a basic bot that rolls dice
 """
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='-', description=description, intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user: return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 
-client.run(os.getenv('BOT_TOKEN'))
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+
+bot.run(os.getenv('BOT_TOKEN'))
